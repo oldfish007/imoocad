d933748d46cd43c447014e1189b59322c3192699
黄芳 <huangfang@huangfangdeMacBook-Pro.local>
2019-04-23 23:28:15 +0800

    1

--- imooc-ad-service/ad-search/src/main/java/com/imooc/ad/index/keyword/UnitKeywordIndex.java
+++ imooc-ad-service/ad-search/src/main/java/com/imooc/ad/index/keyword/UnitKeywordIndex.java
@@ -3,8 +3,12 @@ package com.imooc.ad.index.keyword;
 import com.imooc.ad.index.IndexWare;
 import com.imooc.ad.utils.CommonUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
@@ -29,13 +33,20 @@ public class UnitKeywordIndex implements IndexWare<String, Set<Long>> {//保存u
     }
     @Override
     public Set<Long> get(String key) {
-        return null;
+        if(StringUtils.isEmpty(key)){
+            return Collections.emptySet();
+        }
+        Set<Long> result = keywordUnitMap.get(key);
+        if(result==null){
+            return Collections.emptySet();
+        }
+        return result;
     }
 
     @Override
     public void add(String key, Set<Long> value) {
         log.info("UnitKeyWordIndex,before add:{}",unitKeywordMap);
-        //我们传进来的这个key有可能在keywordUnitMap还没有
+        //我们传进来的这个key有可能在keywordUnitMap还没有Map<String,Set<Long>>
         //如果不存在就把
         Set<Long> unitIdSet  = CommonUtils.getorCreate(
                 key,keywordUnitMap,
@@ -61,8 +72,33 @@ public class UnitKeywordIndex implements IndexWare<String, Set<Long>> {//保存u
     @Override
     public void delete(String key, Set<Long> value) {
 
+    log.info("UnitKeywordIndex,before delete:{}",unitKeywordMap);
+        Set<Long> unitIds = CommonUtils.getorCreate(
+                key, keywordUnitMap,
+                ConcurrentSkipListSet::new);
+        unitIds.removeAll(value);
+        for (Long unitId:
+                value ) {
+            Set<String> keywordSet = CommonUtils.getorCreate(unitId, unitKeywordMap,
+                    ConcurrentSkipListSet::new);
+            keywordSet.remove(key);
+
+        }
+        log.info("UnitKeywordIndex, after delete: {}",unitKeywordMap);
     }
 
     //因为这些限制最终会实现对这个推广单元实现匹配 实现匹配方法也很正常
     //匹配过程是传递进来一个推广单元 传递进来许多关键词 去匹配某一个推广单元 是否包含着这些关键词
+    public boolean match(Long unitId, List<String> keywords) {
+
+        if (unitKeywordMap.containsKey(unitId)
+                && CollectionUtils.isNotEmpty(unitKeywordMap.get(unitId))) {
+
+            Set<String> unitKeywords = unitKeywordMap.get(unitId);
+
+            return CollectionUtils.isSubCollection(keywords, unitKeywords);
+        }
+
+        return false;
+    }
 }
