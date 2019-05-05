249d3a9df50e6254e2915d35a1757e5738eb45d0
oldfish007 <47681668+oldfish007@users.noreply.github.com>
2019-05-05 17:48:46 +0800

    常量文件定义

---
+++ imooc-ad-service/ad-search/src/main/java/com/imooc/ad/mysql/constant/Constant.java
@@ -0,0 +1,11 @@
+package com.imooc.ad.mysql.constant;
+
+/**
+ * @author
+ * @description
+ * @date 2019/5/5
+ */
+public class Constant {
+    private static final  String DB_NAME = "imooc_ad_data";
+
+}
